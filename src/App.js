46bca87d5b545d130731e69b14c8c1998a6ebd7a
prateek883 <singhprateek089@gmail.com>
2022-08-21 16:04:28 +0530

    1 file modified, 7 added, 10 deleted

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './components/Home';
-import Country from './components/Country';
-import Countries from './components/allCountries';
-import './css/App.css';
+import './App.css';
 
 const App = () => {
-  return (
-    <React.Fragment>
-      <Router>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/all" exact component={Countries} />
-          <Route path="/all/:country" exact component={Country} />
-        </Switch>
-      </Router>
-    </React.Fragment>
-  );
+  return <div>Hello world</div>;
 };
 
 export default App;
